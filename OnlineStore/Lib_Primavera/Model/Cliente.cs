cff073764ce599c3bad816678cd0c71c4d3caa09
susanaventura <ei12009@fe.up.pt>
2015-10-16 12:33:52 +0100

    new project

---
+++ OnlineStore/Lib_Primavera/Model/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Lib_Primavera.Model
+{
+    public class Cliente
+    {
+        String id;
+        
+    }
+}

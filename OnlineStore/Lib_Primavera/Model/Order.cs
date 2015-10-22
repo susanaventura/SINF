2b09ec270a48ea51eb0501cbff0df950e19432dd
João Neto <joao.neto.feup@gmail.com>
2015-10-22 20:13:59 +0100

    Orders Model

---
+++ OnlineStore/Lib_Primavera/Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Lib_Primavera.Model
+{
+    public class Order
+    {
+        public List<OrderLine> order_lines { get; set; }
+        public float sub_total { get; set; }
+        public float total_discount { get; set; }
+        public float shipping_cost { get; set; }
+        public float final_price { get; set; }
+    }
+}
---
+++ OnlineStore/Lib_Primavera/Model/OrderLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Lib_Primavera.Model
+{
+    public class OrderLine
+    {
+        // TODO Adicionar Produto
+
+        public int quantity { get; set; }
+        public float price { get; set; }
+        public float final_price { get; set; }
+    }
+}
